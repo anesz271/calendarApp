ff1fc496f512329a4333687f981ed498a55fa63a
Varga Sebestyen <h675741@stud.u-szeged.hu>
2023-04-19 20:44:41 +0200

    add db methods

--- OnlineCalendarWPF/MainWindow.xaml.cs
+++ OnlineCalendarWPF/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Ink;
 
 namespace OnlineCalendarWPF
 {
@@ -15,7 +18,16 @@ namespace OnlineCalendarWPF
             InitializeComponent();
 
             MongoCRUD db = new MongoCRUD("OnlineCalendar");
-            db.InsertRecord("Users", new UserModel {  Name = "Esterházy Péter", Email = "estike@gmail.com"});
+            //db.InsertRecord("Users", new UserModel {  Name = "Esterházy Péter", Email = "estike@gmail.com"});
+
+            var recs = db.LoadRecords<UserModel>("Users");
+            //foreach(var rec in recs)
+            //{
+            //    Console.WriteLine(rec.Name);
+            //}
+
+            //var rec = db.LoadRecordById<UserModel>("Users", new Guid("29f5a97e-1b8c-4ab7-873c-12e2d26a3a82"));
+        
         }
     }
 
@@ -36,6 +48,42 @@ namespace OnlineCalendarWPF
             var collection = db.GetCollection<T>(table);
             collection.InsertOne(record);
         }
+
+        // returns all records
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            // SELECT * 
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        // returns one record
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.Find(filter).First();
+        }
+
+        // insert or update
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", id),
+                record,
+                new ReplaceOptions { IsUpsert = true }); // UpdateOptions obsolate 
+        }
+
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            
+            collection.DeleteOne(filter);
+        }
     }
 
     public class UserModel
@@ -50,6 +98,8 @@ namespace OnlineCalendarWPF
     {
         [BsonId]
         public Guid Id { get; set; }
+        [BsonId]
+        public Guid UserId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
@@ -57,9 +107,27 @@ namespace OnlineCalendarWPF
     public class EventModel
     {
         [BsonId]
-        public  Guid Id { get; set; }   
+        public  Guid Id { get; set; }
+        [BsonId]
+        public Guid UserId { get; set; }
+        [BsonId]
+        public Guid CalendarId { get; set; }
+        public string Title { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class Reminder
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        [BsonId]
+        public Guid UserId { get; set; }
+        [BsonId]
+        public Guid EventId { get; set; }
+        public DateTime ScheduledAt { get; set; }
+
     }
 }
